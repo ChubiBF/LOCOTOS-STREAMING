c4263bb7c1271f0a48b17968827f12012f7fbacf
Andres Baldiviezo <andres123bf@gmail.com>
2026-04-26 22:49:34 -0400

    EL CORSSS

--- locotos-auth-service/src/main.ts
+++ locotos-auth-service/src/main.ts
@@ -1,12 +1,14 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import userRouter from './infrastructure/http/Routes/user-router.js'
+import cors from 'cors'
 
 dotenv.config()
 
 const PORT = process.env.PORT ?? 3000
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 
 app.use('/api/auth', userRouter)
