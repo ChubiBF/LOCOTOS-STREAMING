68bfbc4c2381b1352c5e7de30b8fd58666f71c08
Andres Baldiviezo <andres123bf@gmail.com>
2026-04-28 16:43:04 -0400

    arreglado y mejora del envio de Correos

--- locotos-auth-service/src/infrastructure/email/email.service.ts
+++ locotos-auth-service/src/infrastructure/email/email.service.ts
@@ -6,47 +6,64 @@ import { fileURLToPath } from 'node:url'
 const _dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export class EmailService {
-  private async getTemplate (templateName: string, replacements: Record<string, string>): Promise<string> {
-    const filePath = path.join(_dirname, 'email-templates', `${templateName}.html`)
-    let content = await fs.readFile(filePath, 'utf-8')
+  private async getTemplate (templateName: string, replacements: Record<string, string>): Promise<string | null> {
+    try {
+      const filePath = path.join(_dirname, 'email-templates', `${templateName}.html`)
+      let content = await fs.readFile(filePath, 'utf-8')
 
-    Object.entries(replacements).forEach(([key, value]) => {
-      content = content.replace(new RegExp(`{{${key}}`, 'g'), value)
-    })
+      Object.entries(replacements).forEach(([key, value]) => {
+        content = content.replace(new RegExp(`{{${key}}`, 'g'), value)
+      })
 
-    return content
+      return content
+    } catch (e) {
+      console.log(e)
+      return null
+    }
+  }
+
+  async sendEmail ({ to, subject = 'hola te enviamos un correo', html }: { to: string, subject: string, html: string }): Promise<void> {
+    try {
+      if (process.env.EMAIL_USER === undefined || html == null) return
+      await transporter.sendMail({
+        from: `Locotos Streaming <${process.env.EMAIL_USER}>`,
+        to,
+        subject,
+        html
+      })
+    } catch (e) {
+      console.log(e)
+    }
   }
 
   async sendVerificationEmail (to: string, nombre: string, codigo: string): Promise<void> {
-    const html = await this.getTemplate('verification', { nombre, codigo })
-    if (process.env.EMAIL_USER === undefined) return
-    await transporter.sendMail({
-      from: `Locotos Streaming <${process.env.EMAIL_USER}>`,
-      to,
-      subject: 'Verificar Correo para registro',
-      html
-    })
+    try {
+      const html = await this.getTemplate('verification', { nombre, codigo })
+      if (html == null) return
+      const subject = 'Verifica tu correo y estaras listo!'
+      await this.sendEmail({ to, subject, html })
+    } catch (e) {
+      console.log(e)
+    }
   }
 
   async sendPasswordResetEmail (to: string, nombre: string, enlace: string): Promise<void> {
-    const html = await this.getTemplate('password-reset', { nombre, enlace_reset: enlace })
-    if (process.env.EMAIL_USER === undefined) return
-    await transporter.sendMail({
-      from: `"Seguridad de los locotos" <${process.env.EMAIL_USER}>`,
-      to,
-      subject: 'Reestablecer nueva contrasena',
-      html
-    })
+    try {
+      const html = await this.getTemplate('password-reset', { nombre, enlace_reset: enlace })
+      if (html == null) return
+      await this.sendEmail({ to, subject: 'reestablecer contrasena', html })
+    } catch (e) {
+      console.log(e)
+    }
   }
 
   async sendWelcomeEmail (to: string, nombre: string, enlace: string): Promise<void> {
-    const html = await this.getTemplate('welcome', { nombre, enlace_app: enlace })
-    if (process.env.EMAIL_USER === undefined) return
-    await transporter.sendMail({
-      from: `"Bienvenida de los locotos" <${process.env.EMAIL_USER}>`,
-      to,
-      subject: 'Bienvenida a Locotos Streaming',
-      html
-    })
+    try {
+      const html = await this.getTemplate('welcome', { nombre, enlace_app: enlace })
+      if (html == null) return
+      await this.sendEmail({ to, subject: 'Bienvenida a la plataforma de Locotos!', html })
+    } catch (e) {
+      console.log(e)
+    }
   }
 }
